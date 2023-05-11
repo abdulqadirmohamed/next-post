6ef662d5d91f68ca484fa80bae4e6509c31587a4
Abdulqadir mohamed <63993502+abdulqadirmohamed@users.noreply.github.com>
2023-05-11 08:20:38 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-[#0F0F0F] w-full h-full">
+    <div className="bg-[#0F0F0F] w-full h-screen">
       <div className="w-[85%] sm:w-[45%] mx-auto text-gray-100 py-10">
         <Header />
         <Form/>
